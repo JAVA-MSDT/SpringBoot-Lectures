be0db4dfb7f3d105e1380343ed5c894d79ff317d
Ahmed Samy <serenitydiver@hotmail.com>
2025-06-22 15:32:56 +0200

    Testing spring data and API

--- address/src/main/java/com/mycompany/address/service/AddressService.java
+++ address/src/main/java/com/mycompany/address/service/AddressService.java
@@ -1,7 +1,11 @@
 package com.mycompany.address.service;
 
 import com.mycompany.address.model.Address;
+import com.mycompany.address.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,19 +13,26 @@ import java.util.UUID;
 @Service
 public class AddressService {
 
-    public List<Address> getAddresses() {
-        return List.of(
-                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0000"), "Palestine", "Jerusalem", "Freedom ST"),
-                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0001"), "Egypt", "Cairo", "Pyramids ST"),
-                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0002"), "Russia", "Saint Petersburg", "The kings ST"),
-                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0003"), "Poland", "Warsaw", "Wolska ST, 000")
+    @Autowired
+    private AddressRepository addressRepository;
 
-        );
+    public List<Address> getAddresses() {
+        return addressRepository.findAll();
     }
 
     public Address getAddressById(UUID id) {
-        return getAddresses()
-                .stream()
-                .filter(address -> address.getId().equals(id)).findFirst().orElseThrow();
+        return addressRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Address Not Found in DB."));
     }
+
+//    public List<Address> getAddresses() {
+//        return List.of(
+//                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0000"), "Palestine", "Jerusalem", "Freedom ST"),
+//                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0001"), "Egypt", "Cairo", "Pyramids ST"),
+//                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0002"), "Russia", "Saint Petersburg", "The kings ST"),
+//                new Address(UUID.fromString("bb3373e2-1abc-4961-861e-ce7c38ab0003"), "Poland", "Warsaw", "Wolska ST, 000")
+//
+//        );
+//    }
+
 }
